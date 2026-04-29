24beaed5044542e85edc4b1a8644446f1651dde3
Daniel-Hanrahan-Tools-and-Games <106982630+Daniel-Hanrahan-Tools-and-Games@users.noreply.github.com>
2026-04-28 20:16:32 -0400

    The mod itself

---
+++ Untitled_Knight_Game_Compatibility_Mod/Untitled_Knight_Game_Compatibility_Mod.lua
@@ -0,0 +1,27 @@
+mod =
+{
+    title = "Untitled_Knight_Game_Compatibility_Mod",
+
+    video = "gameplay_video.ogg",
+
+    commands =
+    {
+        {2,"DOWN"},
+        {4,"UP"},
+        {6,"ACTION"},
+        {9,"RIGHT"},
+        {12,"UP"},
+        {15,"ACTION"},
+        {18,"DOWN"},
+        {21,"RIGHT"},
+        {24,"ACTION"},
+        {28,"UP"},
+        {32,"ACTION"},
+        {36,"RIGHT"},
+        {40,"DOWN"},
+        {44,"ACTION"},
+        {48,"UP"},
+        {52,"RIGHT"},
+        {56,"ACTION"}
+    }
+}
